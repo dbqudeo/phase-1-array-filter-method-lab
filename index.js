e92297334d535eb2937c8a6014bda10345f41e72
byeongdae yu <brianbdyu@gmail.com>
2022-05-29 20:40:02 -0400

    commit tree 8d0665a80c9d07cc5f9b52e508665dad26f40e6e

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Code your solution here
+function findMatching(drivers, name){
+    const result = drivers.filter(driver => driver.toUpperCase() === name.toUpperCase());
+    return result;
+}
+
+function fuzzyMatch(drivers, name){
+    const result = drivers.filter(driver => driver.startsWith(name))
+    return result;
+}
+
+function matchName(drivers, name){
+    const result = drivers.filter(driver => driver.name === name)
+    return result;
+}
+/*
+  describe('matchName()', function () {
+    it('accesses the data structure to check if name matches', function () {
+      const drivers = [
+        {
+          name: 'Bobby',
+          hometown: 'Pittsburgh' },
+        {
+          name: 'Sammy',
+          hometown: 'New York' } ,
+        {
+          name: 'Sally',
+          hometown: 'Cleveland' },
+        {
+          name: 'Annette',
+          hometown: 'Los Angeles' },
+        {
+          name: 'Bobby',
+          hometown: 'Tampa Bay' }
+      ];
+
+      expect(matchName(drivers, 'Bobby')).to.eql([
+        {
+          name: 'Bobby',
+          hometown: 'Pittsburgh'
+        },
+        {
+          name: 'Bobby',
+          hometown: 'Tampa Bay'
+        }
+      ]);
+    });
+  });
+});
+*/
